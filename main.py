610bbd9b072dfba5be84ee14b16d31b533f1a0ce
sercavale96 <sercavale@hotmail.com>
2024-07-18 18:25:43 -0500

    Eliminando init de uvicorn

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def hello():
     
     #return {'prediction': predicted_class}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)    
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=8000)    
